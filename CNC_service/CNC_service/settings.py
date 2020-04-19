0977e10ebca733056191f0591ebe61db86ed7a86
Sergey <45179760+NOface312@users.noreply.github.com>
2020-04-19 22:58:06 +0300

    fix

--- CNC_service/CNC_service/settings.py
+++ CNC_service/CNC_service/settings.py
@@ -140,7 +140,6 @@ REST_FRAMEWORK = {
     ),  #
 }
 
-<<<<<<< HEAD
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
@@ -156,26 +155,7 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
-CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_HTTPONLY = False
-=======
-
-SIMPLE_JWT = {
-'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
-'ROTATE_REFRESH_TOKENS': True,
-'BLACKLIST_AFTER_ROTATION': True,
-'ALGORITHM': 'HS256',
-'SIGNING_KEY': SECRET_KEY,
-'VERIFYING_KEY': None,
-'AUTH_HEADER_TYPES': ('JWT',),
-'USER_ID_FIELD': 'id',
-'USER_ID_CLAIM': 'user_id',
-'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-'TOKEN_TYPE_CLAIM': 'token_type',
-} 
 
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 CORS_ORIGIN_ALLOW_ALL = True
->>>>>>> c5d4f43d3013c72e9a66016ad0d932e0a8f72b7f
